8f072a93f985efd271d0252de75e15ce72134f97
juarez <d.juarez@dkfz-heidelberg.de>
2023-05-11 11:12:27 +0200

    First Version

---
+++ src/test/java/de/samply/qualityreportgenerator/QualityReportGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.samply.qualityreportgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityReportGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
